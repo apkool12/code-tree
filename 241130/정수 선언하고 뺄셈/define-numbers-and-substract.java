6407a7a7bdec070a2a3366cc16eba0e333d53a94
apkool12 <147393403+apkool12@users.noreply.github.com>
2024-11-30 17:46:36 +0900

    Title: 정수 선언하고 뺄셈, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241130/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int n1 = 97;
+        int n2 = 13;
+        System.out.print(n1 + " - " + n2 + " = " + (n1 - n2));
+    }
+}
