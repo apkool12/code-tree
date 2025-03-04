bb1e1f76a2fcb9351e9b552a3e6bb9e68d45b4c9
apkool12 <147393403+apkool12@users.noreply.github.com>
2025-03-04 21:14:49 +0900

    Title: 정수 명령 처리 2, Time: 176ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 250304/정수 명령 처리 2/process-numeric-commands-2.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+N = int(input())
+queue = deque()
+
+for _ in range(N):
+    command = input().split()
+    
+    if command[0] == "push":
+        queue.append(int(command[1]))
+    elif command[0] == "pop":
+        print(queue.popleft() if queue else -1)
+    elif command[0] == "front":
+        print(queue[0] if queue else -1)
+    elif command[0] == "size":
+        print(len(queue))
+    elif command[0] == "empty":
+        print(1 if not queue else 0)
