75deefb20b8a2c27783375629ddb633cb88ad960
dmd-sik <147393403+apkool12@users.noreply.github.com>
2025-07-23 10:50:59 +0900

    Title: 합과 평균, Time: 177ms, Memory: 15MB, Status: Passed - Codetree

--- 250723/합과 평균/sum-and-avg.js
+++ 250723/합과 평균/sum-and-avg.js
@@ -4,4 +4,5 @@ var input = fs.readFileSync(0).toString().trim().split(" ");
 var a = Number(input[0]);
 var b = Number(input[1]);
 
-console.log(`${a+b} ${(a+b)/2}`)
+
+console.log(`${a+b} ${((a+b)/2).toFixed(1)}`)
