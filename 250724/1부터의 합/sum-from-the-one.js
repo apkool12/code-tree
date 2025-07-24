b49f69f38bbc421022efed3a121fbc04976038dd
dmd-sik <147393403+apkool12@users.noreply.github.com>
2025-07-24 16:15:26 +0900

    Title: 1부터의 합, Time: 174ms, Memory: 15MB, Status: Passed - Codetree

--- 250724/1부터의 합/sum-from-the-one.js
+++ 250724/1부터의 합/sum-from-the-one.js
@@ -2,12 +2,12 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim();
 
 const num = Number(input);
-var i = 0;
-var total = 0;
+let i = 0;
+let total = 0;
 
-while (total <= num) {
-    i+=1
-    total += i
+while (total < num) {
+  i += 1;
+  total += i;
 }
 
-console.log(i)
+console.log(i);
