136e3c651c328b0115cb3a9fce0a7901f5794384
dmd-sik <147393403+apkool12@users.noreply.github.com>
2025-07-27 01:56:22 -0700

    Title: 숫자 피라미드, Time: 173ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250727/숫자 피라미드/number-pyramid.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const n = Number(input);
+
+for (let i = 1; i <= n; i++) {
+    str = '';
+    for (let j = 1; j <= i; j++) {
+        str += `${i}` + " ";
+    }
+    console.log(str);
+}
