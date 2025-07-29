8c58902f7215a9e8511ae87a39023a60f25594e3
dmd-sik <147393403+apkool12@users.noreply.github.com>
2025-07-29 22:24:29 +0900

    Title: 4가지 관계연산자, Time: 178ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250729/4가지 관계연산자/four-relational-operators.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const nums = input.split(" ").map(Number);
+
+if (nums[0] >= nums[1]) {
+    console.log(1);
+} else {
+    console.log(0);
+}
+if (nums[0] > nums[1]) {
+    console.log(1);
+} else {
+    console.log(0);
+}
+if (nums[0] <= nums[1]) {
+    console.log(1);
+} else {
+    console.log(0);
+}
+if (nums[0] < nums[1]) {
+    console.log(1);
+} else {
+    console.log(0);
+}
