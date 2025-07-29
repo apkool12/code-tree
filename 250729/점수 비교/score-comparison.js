67bfd661ad041ebba8fd57f7ca1af1138378fa4e
dmd-sik <147393403+apkool12@users.noreply.github.com>
2025-07-29 22:38:39 +0900

    Title: 점수 비교, Time: 178ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250729/점수 비교/score-comparison.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const a = input[0].split(" ").map(Number);
+const b = input[1].split(" ").map(Number);
+
+if (a[0] > b[0] && a[1] > b[1]) {
+    console.log(1);
+} else {
+    console.log(0);
+}
