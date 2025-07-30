7b5710dec67757d87dc9559610dcd79eeb6384f3
dmd-sik <147393403+apkool12@users.noreply.github.com>
2025-07-30 09:53:12 +0900

    Title: 수의 계절은, Time: 170ms, Memory: 15MB, Status: Passed - Codetree

--- 250730/수의 계절은/season-of-num.js
+++ 250730/수의 계절은/season-of-num.js
@@ -1,8 +1,14 @@
-const fs = requier("fs");
+const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim();
 
-const month = Number(input);
+let month = Number(input);
 
-if (3 <= month <= 5) {
+if (month >= 3 && month <= 5) {
     console.log("Spring");
+} else if (month >= 6 && month <= 8) {
+    console.log("Summer");
+} else if (month >= 9 && month <= 11) {
+    console.log("Fall");
+} else {
+    console.log("Winter");
 }
