70647f84cb5cf900a0f20f71ba7550de134ac05d
dmd-sik <147393403+apkool12@users.noreply.github.com>
2025-07-30 09:06:55 +0900

    Title: 특정 조건 세 정수 비교, Time: 173ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250730/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const nums = input.split(" ").map(Number);
+
+str = "";
+
+if (nums[1] < nums[2]) {
+    if (nums[0] === nums[1]) {
+        str += 1 + " ";
+    } else {
+        str += 0 + " ";
+    }
+} else {
+    if (nums[0] === nums[2]) {
+        str += 1 + " ";
+    } else {
+        str += 0 + " ";
+    }
+}
+
+if (nums[0] === nums[1] === nums[2]) {
+    str += 1;
+} else {
+    str += 0;
+}
+
+console.log(str);
