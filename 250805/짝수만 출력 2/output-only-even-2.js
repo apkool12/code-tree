12accac1d147142cdad043d7db9a3b89807f6b02
dmd-sik <147393403+apkool12@users.noreply.github.com>
2025-08-05 22:23:06 +0900

    Title: 짝수만 출력 2, Time: 169ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250805/짝수만 출력 2/output-only-even-2.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+let nums = input.split(" ").map(Number);
+const arr = []
+
+while (nums[0] >= nums[1]) {
+    if (nums[0] % 2 === 0) {
+        arr.push(nums[0])
+    }
+    nums[0] -= 1;
+}
+
+console.log(arr.join(" "));
